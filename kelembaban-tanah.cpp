2f7fed3201888daa356a61b62294fe6d5df6e35c
idfp <bashed@tuta.io>
2023-07-04 21:45:36 +0700

    siram di jam 07 dan 16

--- kelembaban-tanah.cpp
+++ kelembaban-tanah.cpp
@@ -2,14 +2,15 @@
 #include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
+#include <cmath>
 #include <iostream>
 
-#define TIME_FACTOR 10 // Faktor percepatan waktu
+#define TIME_FACTOR 5000000 // Faktor percepatan waktu
 
 int i;
-int kelembapan = 40;
+int kelembapan = 80;
 char *status;
-clock_t current_time = clock(); // Waktu saat ini	
+clock_t start_time = clock(); // Waktu awal simulasi
 
 void clearScreen() {
 	#if defined(__linux__)
@@ -19,9 +20,9 @@ void clearScreen() {
 	#endif
 }
 
-void alat_aktif(clock_t start_time){
+void alat_aktif(){
 	clock_t current_time = clock(); // Waktu saat ini
-	double elapsed_time = (double)(current_time - start_time) / CLOCKS_PER_SEC * 10000;
+	double elapsed_time = (double)(current_time - start_time) / CLOCKS_PER_SEC;
 
 	// Menghitung waktu yang dipercepat
 	double accelerated_time = elapsed_time * TIME_FACTOR;
@@ -32,7 +33,9 @@ void alat_aktif(clock_t start_time){
 	int second = (int)accelerated_time % 60;
 
 	// Menampilkan waktu dalam format jam:menit:detik
-
+	if(hour >= 24){
+		hour -= 24 * std::floor(hour / 24);
+	}
 	// Membersihkan layar konsol
     clearScreen();
 	//Menyala
@@ -62,9 +65,9 @@ void alat_aktif(clock_t start_time){
 	printf("      \\_____________/              |______| \n");
 }
 
-void alat_tidakaktif(clock_t start_time){
+void alat_tidakaktif(){
 	clock_t current_time = clock(); // Waktu saat ini
-	double elapsed_time = (double)(current_time - start_time) / CLOCKS_PER_SEC * 10000;
+	double elapsed_time = (double)(current_time - start_time) / CLOCKS_PER_SEC;
 
 	// Menghitung waktu yang dipercepat
 	double accelerated_time = elapsed_time * TIME_FACTOR;
@@ -73,13 +76,29 @@ void alat_tidakaktif(clock_t start_time){
 	int hour = (int)accelerated_time / 3600;
 	int minute = ((int)accelerated_time % 3600) / 60;
 	int second = (int)accelerated_time % 60;
-
+	if(hour >= 24){
+		hour -= 24 * std::floor(hour / 24);
+	}
 	// Menampilkan waktu dalam format jam:menit:detik
-
+	if(hour == 7 || hour == 16){
+		int placeholder = hour;
+		for(i = kelembapan; i<=80; i+=30){	
+			status = "Keran Menyala";
+			if(i < 80){
+				i += 20
+			}
+			kelembapan = i;
+			alat_aktif();
+			sleep(1);
+			if(hour > placeholder + 1){
+				break;
+			}
+		}
+	}
 	// Membersihkan layar konsol
     clearScreen();
 	//Menyala
-	printf("%02d:%02d:%02d     _ \n", hour, minute, second);
+	printf("%02d:%02d:%02d      \n", hour, minute, second);
 	//Tidak Menyala
 	printf("             _ \n");
 	printf("           _|_|_ \n");
@@ -107,26 +126,22 @@ void alat_tidakaktif(clock_t start_time){
 	printf("      \\_____________/              |______| \n");	
 }
 int main (){
-	clock_t start_time = clock(); // Waktu awal simulasi
 
 	while(true){
 		clearScreen();
-		if(kelembapan == 40){
-			for(i = 40; i<=80; i+=2){	
-				status = "Keran Menyala";
-				kelembapan = i;
-				alat_aktif(start_time);
-				sleep(1);
-			}
-		if(kelembapan == 80){
-			for(i = 80; i>=40; i-=2){
-				status = "  Keran Mati ";
-				kelembapan = i;
-				alat_tidakaktif(start_time);
-				sleep(1);
-			}
-		}
-		kelembapan = 40;			
-	}
+		// if(kelembapan == 40){
+		// 	for(i = 40; i<=80; i+=2){	
+		// 		status = "Keran Menyala";
+		// 		kelembapan = i;
+		// 		alat_aktif(start_time);
+		// 		sleep(1);
+		// 	}
+		// }
+		// if(kelembapan == 80){
+		status = "  Keran Mati ";
+		kelembapan -= 2;
+		alat_tidakaktif();
+		sleep(1);
+		// }
 	}
 }
